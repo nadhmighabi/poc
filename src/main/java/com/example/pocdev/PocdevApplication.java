1ba9d5e2937ff76c9db39ff01b6a245bfe6c60d7
Amal1999 <sammari_a@outlook.fr>
2024-10-19 19:41:04 +0100

    first commit

---
+++ src/main/java/com/example/pocdev/PocdevApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pocdev/PocdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
